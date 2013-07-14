751efdd03cc0d37ee4fc7d0f83d21f2dda5ef0cc
chrisrw <chrisrw@hungryfishmedia.com>
2013-07-14 16:42:42 -0400

    Fix input tag W3C validator

--- wp-content/themes/chefscollaborative/newsletter.php
+++ wp-content/themes/chefscollaborative/newsletter.php
@@ -5,7 +5,7 @@
 		<fieldset>
 		<input type="hidden" name="m" value="1101685454097" />
 		<input type="hidden" name="p" value="oi" />
-		<input class="email cleardefault" type="text" value="enter your email address" name="ea" style="color: rgb(153, 153, 153);">
+		<input class="email cleardefault" type="text" value="enter your email address" name="ea" style="color: rgb(153, 153, 153);" />
 		<input type="image" src="<?php bloginfo('template_url'); ?>/images/icons-mail.png" class="ok" name="go" value="OK" style="border: 0;" />
 		</fieldset>
 	</form>
