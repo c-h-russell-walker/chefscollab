00b5462bd06cfd9266e50b59592828af92b63d6c
chrisrw <chrisrw@hungryfishmedia.com>
2013-07-15 22:25:55 -0400

    Remove ref to favicon for now

--- wp-content/themes/chefscollaborative/header.php
+++ wp-content/themes/chefscollaborative/header.php
@@ -11,7 +11,7 @@
 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
-<link rel="shortcut icon" type="image/ico" href="<?php bloginfo('template_url'); ?>/favicon.ico" />
+<!--<link rel="shortcut icon" type="image/ico" href="<?php bloginfo('template_url'); ?>/favicon.ico" />-->
 
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
