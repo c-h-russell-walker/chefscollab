7cc8702036c73ed59554e5745e2a1e11cd6db2d8
chrisrw <chrisrw@hungryfishmedia.com>
2013-07-18 10:11:19 -0400

    Update sidebar nav display

--- wp-content/themes/chefscollaborative/footer.php
+++ wp-content/themes/chefscollaborative/footer.php
@@ -20,5 +20,11 @@
 var _gaq = _gaq || [];   _gaq.push(['_setAccount', 'UA-37131868-1']);   _gaq.push(['_setDomainName', 'chefscollaborative.org']);   _gaq.push(['_trackPageview']);   (function() {     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);   })();
 // ]]></script>
 
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script>
+	if (!$('.current_page_item').hasClass('page_folder'))
+		$('.nav-cont').hide();
+</script>
+
 </body>
 </html>
