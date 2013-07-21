1b4202e7764286b606c150b0650d26f567632057
chrisrw <chrisrw@hungryfishmedia.com>
2013-07-21 11:51:35 -0400

    Validation fixes

--- wp-content/themes/chefscollaborative/footer.php
+++ wp-content/themes/chefscollaborative/footer.php
@@ -20,12 +20,12 @@
 var _gaq = _gaq || [];   _gaq.push(['_setAccount', 'UA-37131868-1']);   _gaq.push(['_setDomainName', 'chefscollaborative.org']);   _gaq.push(['_trackPageview']);   (function() {     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);   })();
 // ]]></script>
 
- <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-<script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script type="text/javascript">// <![CDATA[
 	if (!$('#left .current_page_item').hasClass('page_folder') && 
 		!$('#left .current_page_item').parents().hasClass('child-list'))
 		$('.nav-cont').hide();
-</script>
+// ]]></script>
 
 </body>
 </html>
